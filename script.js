50c1a519fadb76d6d0f1bb7c9090ed2783994a9e
Sergio Valadez <sergioval@gmail.com>
2022-04-02 10:51:54 -0600

    Initiate js file

---
+++ script.js
@@ -0,0 +1,4 @@
+const body = document.body;
+const slides = document.quesrySelectorAll('.slide');
+const leftBtn = document.getElementById('left-arrow'); 
+const rightBtn = document.getElementById('right-arrow');
